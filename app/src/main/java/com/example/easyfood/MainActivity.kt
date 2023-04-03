ede0e9fa6bef096f5b748182bcc70c500a803ecc
Nursultan <nursamanzhol@gmail.com>
2023-04-03 23:38:41 +0600

    не исправленный баг

--- app/src/main/java/com/example/easyfood/MainActivity.kt
+++ app/src/main/java/com/example/easyfood/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.easyfood
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.easy_food.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
